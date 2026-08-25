e119d157c09eec9b1343eb6711d71eb45df56ad8
Pedro Martins <pedromartinsss996@gmail.com>
2026-08-25 20:34:37 -0300

    Criação de Lista de Tarefas com ListBuilder - Fase B

--- lib/lista_tarefas_page.dart
+++ lib/lista_tarefas_page.dart
@@ -5,97 +5,54 @@ class ListaTarefasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+  final List<Map<String, dynamic>> tarefas = [
+
+    {'titulo' : 'Configuração do Ambiente', 'situacao' : true},
+    {'titulo' : 'Fazer compras', 'situacao' : false},
+    {'titulo' : 'Estudar inglês', 'situacao' : false},
+    {'titulo' : 'Fazer compras', 'situacao' : true},
+    {'titulo' : 'Pagar a fatura', 'situacao' : true},
+    {'titulo' : 'Sair as 22h10', 'situacao' : false},
+  ];
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas Tarefas"),
         centerTitle: true,
       ),
 
-      body: ListView(
+      body: ListView.builder(
         padding: EdgeInsets.all(12),
-        children: [
-          Card(
+        itemCount: tarefas.length,
+        itemBuilder: (context, index) {
+
+          final tarefa = tarefas[index];
+          final bool situacao = tarefa['situacao'];
+
+          return Card(
             margin: EdgeInsets.symmetric(vertical: 6),
             child: ListTile(
               leading: Icon(
-                Icons.check_circle,
-                color: Colors.green,
+                situacao ? Icons.check_circle : Icons.circle_outlined,
+                color: situacao ? Colors.green : Colors.grey,
               ),
               title: Text(
-                'Configurar o Ambiente de Desenvolvimento',
+                tarefa['titulo'],
                 style: TextStyle(
-                  decoration: TextDecoration.lineThrough,
+                  decoration: situacao ? TextDecoration.lineThrough : TextDecoration.none,
                 ),
               ),
 
-              subtitle: Text('Concluida'),
+              subtitle:  Text(situacao ?'Concluida' : 'Pendente'),
               trailing: Icon(
                 Icons.delete_outline,
                 color: Colors.grey,
               ),
             ),
-          ),
-
-          Card(
-            margin: EdgeInsets.symmetric(vertical: 6),
-            child: ListTile(
-              leading: Icon(
-                Icons.circle_outlined,
-                color: Colors.grey,
-              ),
-              title: Text(
-                'Criar Projeto em Flutter',
-              ),
-
-              subtitle: Text('Pendente'),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-
-          Card(
-            margin: EdgeInsets.symmetric(vertical: 6),
-            child: ListTile(
-              leading: Icon(
-                Icons.check_circle,
-                color: Colors.blue,
-              ),
-              title: Text(
-                ' Iniciar Desenvolvimento do App de Compras',
-                style: TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                ),
-              ),
-
-              subtitle: Text('Concluida com sucesso'),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-
-          Card(
-            margin: EdgeInsets.symmetric(vertical: 6),
-            child: ListTile(
-              leading: Icon(
-                Icons.circle_outlined,
-                color: const Color.fromARGB(255, 206, 15, 63),
-              ),
-              title: Text(
-                'FInalizar App de Compras',
-              ),
-
-              subtitle: Text('Tarefa Futura'),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-        ],
+          );
+        }
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {},
       child: Icon(Icons.add),
